fcc7ae9c4852a50162f3dcf2813ee2636a79d9c0
V3g0nAR <alexvegonar@gmail.com>
2020-06-25 14:05:44 +0100

    Updates

---
+++ precache-manifest.855d665f9deaf8f18378f794c28a1de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48416443aebd1b6fa2c293eb8c2a2f0",
+    "url": "/add-info-to-table-using-form-react/index.html"
+  },
+  {
+    "revision": "4c77024b6ce554032819",
+    "url": "/add-info-to-table-using-form-react/static/css/main.c5117ac6.chunk.css"
+  },
+  {
+    "revision": "7cdc324ba5f9714a18a1",
+    "url": "/add-info-to-table-using-form-react/static/js/2.7566af9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/add-info-to-table-using-form-react/static/js/2.7566af9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c77024b6ce554032819",
+    "url": "/add-info-to-table-using-form-react/static/js/main.56efb876.chunk.js"
+  },
+  {
+    "revision": "fd432fe16a634d5a396c",
+    "url": "/add-info-to-table-using-form-react/static/js/runtime-main.e36794d3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/add-info-to-table-using-form-react/precache-manifest.855d665f9deaf8f18378f794c28a1de7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/add-info-to-table-using-form-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
